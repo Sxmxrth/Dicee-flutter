cff976bed9a3b795a23cac605a8469f034006c1b
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-02 22:41:48 +0530

    Dice added

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +9,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -18,7 +21,31 @@ void main() {
 
 class DicePage extends StatelessWidget {
   @override
+  var val = 1;
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                val = Random().nextInt(5);
+                print("Left button pressed");
+              },
+              child: Image.asset("images/dice${val + 1}.png"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print("Right button pressed");
+                val = Random().nextInt(6);
+              },
+              child: Image.asset("images/dice${val + 1}.png"),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
