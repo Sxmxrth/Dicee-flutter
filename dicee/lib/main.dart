05e95b10cd86ebff9dc089e110c9fab018c94655
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-02 23:27:50 +0530

    App completer

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -19,9 +19,25 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
   @override
-  var val = 1;
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftClick = 0;
+  var rightClick = 0; //These must be outside the widget build
+
+  void changeDiceFace() {
+    setState(() {
+      leftClick = Random().nextInt(6) + 1;
+      rightClick = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
@@ -29,19 +45,17 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: TextButton(
               onPressed: () {
-                val = Random().nextInt(5);
-                print("Left button pressed");
+                changeDiceFace();
               },
-              child: Image.asset("images/dice${val + 1}.png"),
+              child: Image.asset("images/dice$leftClick.png"),
             ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("Right button pressed");
-                val = Random().nextInt(6);
+                changeDiceFace();
               },
-              child: Image.asset("images/dice${val + 1}.png"),
+              child: Image.asset("images/dice$rightClick.png"),
             ),
           )
         ],
